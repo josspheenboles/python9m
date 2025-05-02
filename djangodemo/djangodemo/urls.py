adade102121b427c3c263870feb621aefe7eb607
josspheenboles <eng.jospheenboles@gmail.com>
2025-05-02 23:26:34 +0300

    template block

--- djangodemo/djangodemo/urls.py
+++ djangodemo/djangodemo/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from product.views import *
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',home,name='home'),
