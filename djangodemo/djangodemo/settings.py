9c9558ad0bd1fdc2758a30c2b1bc714fde4763ad
josspheenboles <eng.jospheenboles@gmail.com>
2025-05-02 21:42:09 +0300

    apps configuration

--- djangodemo/djangodemo/settings.py
+++ djangodemo/djangodemo/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'product',
+    'catagory.apps.CatagoryConfig',
+    'account.apps.AccountConfig',
 ]
 
 MIDDLEWARE = [
