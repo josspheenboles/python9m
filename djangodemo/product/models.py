1009fc25e0765a159cc26d9784d6d65f6538e455
josspheenboles <eng.jospheenboles@gmail.com>
2025-05-02 22:21:33 +0300

    media configuration

--- djangodemo/product/models.py
+++ djangodemo/product/models.py
@@ -5,5 +5,5 @@ class Product(models.Model):
     id=models.AutoField(primary_key=True)
     name=models.CharField(max_length=50,null=False,unique=False)
     price=models.DecimalField(max_digits=2,decimal_places=1)
-    image=models.ImageField(upload_to='media/product/images/',null=True,blank=True)
+    image=models.ImageField(upload_to='product/images/',null=True,blank=True)
     isactive=models.BooleanField(default=True)
