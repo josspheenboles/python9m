c623a42a6ab0296c8548155ba7b279290f170f71
josspheenboles <eng.jospheenboles@gmail.com>
2025-05-03 00:17:50 +0300

    partial urls & send data in url

--- djangodemo/product/urls.py
+++ djangodemo/product/urls.py
@@ -4,5 +4,9 @@ from django.urls import path
 from .views import *
 urlpatterns=[
     path('',productlist,name='plist'),
+    path('Add/',productadd,name='padd'),
+    path('Update/<id:int>',productupdate,name='pupdate'),
+    path('Delete/<id:int>',productdel,name='pdel'),
+
 
 ]
