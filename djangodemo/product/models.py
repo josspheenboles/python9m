3b07a5788ea218ef83855fabc069f7a3d044d5b8
josspheenboles <eng.jospheenboles@gmail.com>
2025-05-02 22:13:47 +0300

    product images

--- djangodemo/product/models.py
+++ djangodemo/product/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    id=models.AutoField(primary_key=True)
+    name=models.CharField(max_length=50,null=False,unique=False)
+    price=models.DecimalField(max_digits=2,decimal_places=1)
+    image=models.ImageField(upload_to='media/product/images/',null=True,blank=True)
+    isactive=models.BooleanField(default=True)
