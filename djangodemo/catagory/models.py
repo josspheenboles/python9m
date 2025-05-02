c14b663a3a96174b5dc2090abfe540f615ac51ff
josspheenboles <eng.jospheenboles@gmail.com>
2025-05-02 21:45:51 +0300

    catagory model

--- djangodemo/catagory/models.py
+++ djangodemo/catagory/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+import uuid
 # Create your models here.
+class Category(models.Model):
+    """Hierarchical category system with advanced features"""
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, max_length=150)
+    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='categories/', null=True, blank=True)
--- djangodemo/djangodemo/settings.py
+++ djangodemo/djangodemo/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # not professional
     'product',
     'catagory.apps.CatagoryConfig',
     'account.apps.AccountConfig',
