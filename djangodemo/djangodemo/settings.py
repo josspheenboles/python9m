4c4c0fdadb8c11e65f68d44170866b794d598d01
josspheenboles <eng.jospheenboles@gmail.com>
2025-05-02 21:40:29 +0300

    apps creation

--- djangodemo/djangodemo/settings.py
+++ djangodemo/djangodemo/settings.py
@@ -75,19 +75,19 @@ WSGI_APPLICATION = 'djangodemo.wsgi.application'
 DATABASES = {
     'default': {
 
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'djangodb',
-        'USER': 'root',
-        'PASSWORD': '123',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
-            'charset': 'utf8mb4',
-        },
-        'CONN_MAX_AGE': 300,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'djangodb',
+        # 'USER': 'root',
+        # 'PASSWORD': '123',
+        # 'HOST': 'localhost',
+        # 'PORT': '3306',
+        # # 'OPTIONS': {
+        #     'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
+        #     'charset': 'utf8mb4',
+        # },
+        # 'CONN_MAX_AGE': 300,
 
     }
 }
