5e39910badf3c9111abfbcbceea1664ac86762e2
josspheenboles <eng.jospheenboles@gmail.com>
2025-05-02 22:34:54 +0300

    auth configuration

--- djangodemo/account/models.py
+++ djangodemo/account/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-import uuid
+
 # Create your models here.
 class MyUser(AbstractUser):
     """Extended User model with e-commerce specific fields"""
