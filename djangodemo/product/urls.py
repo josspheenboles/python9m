29181501d6aa331219c4fe079213b8e43124a341
josspheenboles <eng.jospheenboles@gmail.com>
2025-05-03 00:27:35 +0300

    template url tag

--- djangodemo/product/urls.py
+++ djangodemo/product/urls.py
@@ -5,8 +5,8 @@ from .views import *
 urlpatterns=[
     path('',productlist,name='plist'),
     path('Add/',productadd,name='padd'),
-    path('Update/<id:int>',productupdate,name='pupdate'),
-    path('Delete/<id:int>',productdel,name='pdel'),
+    path('Update/<int:id>',productupdate,name='pupdate'),
+    path('Delete/<int:id>',productdel,name='pdel'),
 
 
 ]
