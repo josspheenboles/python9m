9778669a9834e4990b93f249a63a7dd2da5fb8df
josspheenboles <eng.jospheenboles@gmail.com>
2025-05-02 22:28:15 +0300

    User model

--- djangodemo/account/models.py
+++ djangodemo/account/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 import uuid
 # Create your models here.
-class User(AbstractUser):
+class MyUser(AbstractUser):
     """Extended User model with e-commerce specific fields"""
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(unique=True, db_index=True)
