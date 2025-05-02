0dcde1887259dd7a405c17cc14744f37b98c649f
josspheenboles <eng.jospheenboles@gmail.com>
2025-05-02 22:28:38 +0300

    User model

--- djangodemo/account/models.py
+++ djangodemo/account/models.py
@@ -4,7 +4,6 @@ import uuid
 # Create your models here.
 class MyUser(AbstractUser):
     """Extended User model with e-commerce specific fields"""
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(unique=True, db_index=True)
     phone = models.CharField(max_length=20, blank=True, null=True)
     date_of_birth = models.DateField(null=True, blank=True)
