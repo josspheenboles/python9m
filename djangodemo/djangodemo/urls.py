387fa00b03f0a4aa06f6547c4c9134435620b85e
josspheenboles <eng.jospheenboles@gmail.com>
2025-05-06 01:08:11 +0300

    Create Token Views

--- djangodemo/djangodemo/urls.py
+++ djangodemo/djangodemo/urls.py
@@ -19,8 +19,12 @@ from django.urls import path,include
 from product.views import *
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',home,name='home'),
-    path('Product/',include('product.urls'))
+    path('Product/',include('product.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
