1bb0609cb1d4db79fc3649f05f667f2864e82c77
josspheenboles <eng.jospheenboles@gmail.com>
2025-05-06 01:05:40 +0300

    jwt

--- djangodemo/djangodemo/settings.py
+++ djangodemo/djangodemo/settings.py
@@ -148,8 +148,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK={
 'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',  # Optional for browsable API
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',  # Optional for browsable API
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',  # Require auth by default
