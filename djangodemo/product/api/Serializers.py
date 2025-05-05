82df2650c885a2722d08cdca58bc1260b1ca8888
josspheenboles <eng.jospheenboles@gmail.com>
2025-05-05 22:49:41 +0300

    product basic serializer

--- djangodemo/product/api/Serializers.py
+++ djangodemo/product/api/Serializers.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
-from catagory.models import Category
-
+from catagory.Serializers import Catagory_Serializer
 
 class Product_serializer(serializers.Serializer):
     id=serializers.IntegerField(read_only=True)
     name = serializers.CharField(max_length=50, null=False, unique=False)
     image = serializers.ImageField(upload_to='product/images/', null=True, blank=True)
     isactive = serializers.BooleanField(default=True)
-    catagoryid = serializers.ForeignKey(Category, on_delete=models.CASCADE)
+    # Nested  Serializers
+    catagory =Catagory_Serializer()
